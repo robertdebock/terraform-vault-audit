d5c39cca49b3e45e3a67e0398ff8db8c02a4a3e3
Robert de Bock <robert@meinit.nl>
2022-05-18 21:08:25 +0200

    Spec the variable.

--- variables.tf
+++ variables.tf
@@ -1,5 +1,10 @@
 variable "audit_devices" {
   description = "A list of audit devices."
+  type = list(object({
+    type = string
+    # local = bool
+    options = map(any)
+  }))
   default = [{
     type  = "file"
     local = true
