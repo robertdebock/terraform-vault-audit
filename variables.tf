670ab1d2fb9d49cf79252f63b562c3e3c4c92901
Robert de Bock <robert@meinit.nl>
2022-05-18 14:06:26 +0200

    It's a list.

--- variables.tf
+++ variables.tf
@@ -1,15 +1,10 @@
 variable "audit_devices" {
   description = "A list of audit devices."
-  # type = set(objects({
-  #   type    = string
-  #   local   = boolean
-  #   options = map
-  # }))
-  default = {
+  default = [{
     type  = "file"
     local = true
     options = {
       file_path = "/var/log/vault_audit.log"
     }
-  }
+  }]
 }
